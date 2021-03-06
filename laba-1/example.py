39604725b46324b6bad92e28789c94a83720c0e5
elilvina <hi.elilvina@gmail.com>
2021-03-06 15:45:28 +0300

    upd example.py

--- laba-1/example.py
+++ laba-1/example.py
@@ -1,5 +1,12 @@
-print ("well, hello there.")
-name = input("what is your name? ")
-print ("it's nice to meet you, " + name + "!")
-age = input("how old are you? ")
-print ("wow, " + age + "! you are at the best age.")
+import logging
+logging.basicConfig(level=logging.INFO)
+
+def main():
+    logging.info("well, hello there.")
+    name = input("what is your name? ")
+    logging.info("it's nice to meet you, " + name + "!")
+    age = input("how old are you? ")
+    logging.info("wow, " + age + "! you are at the best age.")
+
+if __name__=="__main__":
+    main()
